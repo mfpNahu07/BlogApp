9db56fb41ea704c088e0558fb1315f13a3121921
Nahue Imaz <nahuelji.773@gmail.com>
2022-04-26 16:05:09 -0300

    Se crea el ViewModel de capa presentation

---
+++ app/src/main/java/com/nahuel/blogapp/presentation/HomeScreenViewModel.kt
@@ -0,0 +1,34 @@
+package com.nahuel.blogapp.presentation
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
+import com.nahuel.blogapp.core.Resource
+import com.nahuel.blogapp.repository.HomeScreenRepo
+import kotlinx.coroutines.Dispatchers
+import java.lang.Exception
+
+class HomeScreenViewModel(private val repo: HomeScreenRepo) : ViewModel() {
+
+    fun fetchLatestPost() = liveData(Dispatchers.IO) {
+        emit(Resource.Loading())
+        try {
+            emit(repo.getLatestPosts())
+        } catch (e: Exception) {
+            emit(Resource.Failure(e))
+        }
+
+    }
+
+}
+
+//Creamos una instancia del ViewModel a través de la implementación concreta de donde esta
+//ubicada la interfaz, por eso no es  repo: HomeScreenRepoImpl
+
+class HomeScreenViewModelFactory(private val repo: HomeScreenRepo): ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+       return modelClass.getConstructor(HomeScreenRepo::class.java).newInstance(repo)
+    }
+
+}
--- app/src/main/java/com/nahuel/blogapp/ui/home/HomeScreenFragment.kt
+++ app/src/main/java/com/nahuel/blogapp/ui/home/HomeScreenFragment.kt
@@ -5,25 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.nahuel.blogapp.R
+import com.nahuel.blogapp.core.Resource
 import com.nahuel.blogapp.data.model.Post
+import com.nahuel.blogapp.data.remote.HomeScreenDataSource
 import com.nahuel.blogapp.databinding.FragmentHomeScreenBinding
+import com.nahuel.blogapp.presentation.HomeScreenViewModel
+import com.nahuel.blogapp.presentation.HomeScreenViewModelFactory
+import com.nahuel.blogapp.repository.HomeScreenRepoImpl
 import com.nahuel.blogapp.ui.home.adapter.HomeScreenAdapter
 
 
 class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
 
     private lateinit var binding: FragmentHomeScreenBinding
+    private val viewModel by viewModels<HomeScreenViewModel> {
+        HomeScreenViewModelFactory(HomeScreenRepoImpl(HomeScreenDataSource()))
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeScreenBinding.bind(view)
 
+        viewModel.fetchLatestPost().observe(viewLifecycleOwner, Observer { result ->
+            when(result){
+                is Resource.Loading ->{
+                    binding.progressBar.visibility = View.VISIBLE
+                }
 
-        val postList = listOf(Post(), Post(), Post())
+                is Resource.Succcess -> {
+                    binding.progressBar.visibility = View.GONE
+                    binding.rvHome.adapter = HomeScreenAdapter(result.data)
+                }
+
+                is Resource.Failure -> {
+                    binding.progressBar.visibility = View.GONE
+                    Toast.makeText(requireContext(), "Ocurrio un error: ${result.exception}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        })
 
 
-        binding.rvHome.adapter = HomeScreenAdapter(postList)
     }
 
 
