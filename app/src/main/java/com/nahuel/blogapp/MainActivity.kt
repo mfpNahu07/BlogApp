1973ced36cdf613ca01f5a1ca92efa586d259e7f
Nahue Imaz <nahuelji.773@gmail.com>
2022-05-10 19:09:03 -0300

    Se creó navigation menu

--- app/src/main/java/com/nahuel/blogapp/MainActivity.kt
+++ app/src/main/java/com/nahuel/blogapp/MainActivity.kt
@@ -3,14 +3,54 @@ package com.nahuel.blogapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.nahuel.blogapp.core.hide
+import com.nahuel.blogapp.core.show
+import com.nahuel.blogapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        observeDestinationChange()
+
+
     }
+
+    private fun observeDestinationChange() {
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+
+                R.id.loginFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+
+                R.id.registerFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+
+                else -> {
+                    binding.bottomNavigationView.show()
+                }
+            }
+        }
+    }
+
+
 }
 
---
+++ app/src/main/java/com/nahuel/blogapp/ui/camera/CameraFragment.kt
@@ -0,0 +1,18 @@
+package com.nahuel.blogapp.ui.camera
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nahuel.blogapp.R
+
+
+class CameraFragment : Fragment(R.layout.fragment_camera) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
