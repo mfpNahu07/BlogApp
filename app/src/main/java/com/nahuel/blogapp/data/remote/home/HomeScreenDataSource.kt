0e3c59aa028ad30a8e946d775c211a1ab0017f7e
Nahue Imaz <nahuelji.773@gmail.com>
2022-05-27 10:05:23 -0300

    Se ordenan los posts en orden descendiente

--- app/src/main/java/com/nahuel/blogapp/data/remote/home/HomeScreenDataSource.kt
+++ app/src/main/java/com/nahuel/blogapp/data/remote/home/HomeScreenDataSource.kt
@@ -2,6 +2,7 @@ package com.nahuel.blogapp.data.remote.home
 
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.nahuel.blogapp.core.Resource
 import com.nahuel.blogapp.data.model.Post
 import kotlinx.coroutines.tasks.await
@@ -10,7 +11,8 @@ class HomeScreenDataSource {
     suspend fun getLatestPost(): Resource<List<Post>> {
 
         val postList = mutableListOf<Post>()
-        val querySnapshot = FirebaseFirestore.getInstance().collection("posts").get().await()
+        val querySnapshot = FirebaseFirestore.getInstance().collection("posts").orderBy("created_at", Query.Direction.DESCENDING)
+            .get().await()
         for( post in querySnapshot.documents){
             post.toObject(Post::class.java)?.let {
                 it.apply { created_at = post.getTimestamp("created_at", DocumentSnapshot.ServerTimestampBehavior.ESTIMATE)?.toDate() }
